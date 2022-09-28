6d5ed9cc1594f577a89c4ab1c638506b9a3841d2
Jason Lee <jslee423@gmail.com>
2022-09-28 10:47:41 -0400

    adjust certain icon sizes to look better

--- src/components/SkillsIcons.js
+++ src/components/SkillsIcons.js
@@ -4,7 +4,7 @@ const SkillsIcons = ({ img, label }) => {
     return (
         <div className='skillsContainer'>
             <div className='skillsIcon'>
-                <img src={img} alt='html 5 logo' />
+                <img src={img} alt='html 5 logo' style={label === 'EXPRESS JS' || label === 'NATIVE' ? {width: '75%'} : {width: '60%'}} />
             </div>
             <p className='skillLabel mt-2'>{label}</p>
         </div>
