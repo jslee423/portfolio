0e7088bddc957c0de1764787e7f2f3f6112aed32
Jason Lee <jslee423@gmail.com>
2022-11-16 23:08:44 -0500

    add hover tilt to skills icons

--- src/components/SkillsIcons.js
+++ src/components/SkillsIcons.js
@@ -1,9 +1,23 @@
+import { useSpring, animated } from 'react-spring';
 import './SkillsIcons.css';
 
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.02];
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 const SkillsIcons = ({ img, label }) => {
+    const [props, set] = useSpring(() => ({
+        xys: [0, 0, 1],
+        config: { mass: 5, tension: 350, friction: 40 }
+    }));
+
     return (
         <div className='skillsContainer'>
-            <div className='skillsIcon'>
+            <animated.div
+                className='skillsIcon'
+                onMouseMove={({ relatedTarget: x, relatedTarget: y }) => set({ xys: calc(x, y) })}
+                onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                style={{ transform: props.xys.interpolate(trans) }}
+            >
                 <img
                     src={img}
                     alt='html 5 logo'
@@ -13,7 +27,7 @@ const SkillsIcons = ({ img, label }) => {
                         : {width: '60%'}
                     }
                 />
-            </div>
+            </animated.div>
             <p className='skillLabel mt-2'>{label}</p>
         </div>
     );
